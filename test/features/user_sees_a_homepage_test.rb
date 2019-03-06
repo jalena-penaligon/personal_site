1077c86c25b55e4789123ad1c66e5375db24f224
Jalena Penaligon <jalena.penaligon@gmail.com>
2019-03-06 13:09:14 -0700

    Added test for about page

--- test/features/user_sees_a_homepage_test.rb
+++ test/features/user_sees_a_homepage_test.rb
@@ -4,8 +4,22 @@ class HomepageTest < CapybaraTestCase
   def test_user_can_see_the_homepage
     visit '/'
 
-    save_and_open_page
     assert page.has_content?("Welcome!")
     assert_equal 200, page.status_code
   end
+
+  def test_page_not_found_error_displays
+    visit '/blog'
+
+    assert page.has_content?("Page not found.")
+    assert_equal 404, page.status_code
+  end
+
+  def test_user_can_see_about_page
+    visit '/about'
+
+    assert page.has_content?("Here's some stuff to know.")
+    assert_equal 200, page.status_code
+  end
+
 end
